5f27f86db61de1c4c7876f1723eccc7687d14f29
Gabriel Michi <33666688+gabrielmichi@users.noreply.github.com>
2018-03-08 17:02:28 -0300

    Add files via upload

---
+++ Coluna_Todas_tabelas.sql
@@ -0,0 +1,12 @@
+--procurar coluna nas tabelas
+SELECT
+    T.name AS Tabela,
+    C.name AS Coluna
+FROM
+    sys.sysobjects    AS T (NOLOCK)
+INNER JOIN sys.all_columns AS C (NOLOCK) ON T.id = C.object_id AND T.XTYPE = 'U'
+WHERE
+    C.NAME LIKE '%coluna%'
+ORDER BY
+    T.name ASC
+
